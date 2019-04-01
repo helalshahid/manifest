309f2f4906cffd06f6528e1b9454a27cad2f5604
helalshahid <34959644+helalshahid@users.noreply.github.com>
2019-04-01 15:04:07 +0530

    sdfsdf
    dsfdsfds

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ self.addEventListener('fetch', function(e) {
   console.log(e.request.url);
   e.respondWith(
     caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+      return response
     })
   );
 });
